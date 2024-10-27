f78ca10333ea2176a39fd61a69b116589872d27e
ariskoutris <koutrisaris@gmail.com>
2024-10-27 16:16:49 +0100

    Convert notebook to python_script

---
+++ src/pass_tables.py
@@ -0,0 +1,115 @@
+import pandas as pd
+from utils import *
+    
+# Set Wyscout and Skillcorner IDs
+WYSCOUT_ID = 5414111
+SKILLCORNER_ID = 952209
+
+# Set path to zip files. Set to None if files are already unzipped.
+ZIP_PATH = '../../soccer-analytics/data/'
+ZIP_PATH = None
+
+# Set path to save the resulting dataframe. Otherwise, set to None.
+SAVE_PATH = None
+
+DATA_PATH = '../data/'
+WYSCOUT_PATH = DATA_PATH + 'wyscout/'
+SKILLCORNER_PATH = DATA_PATH + 'skillcorner/'
+XT_PLOT_PATH = DATA_PATH + 'smoothed_xt.csv'
+MATCH_IDS_PATH = DATA_PATH + 'matchids.csv'
+
+
+# Load Data
+if ZIP_PATH:
+    unzip_all_files_in_dir(ZIP_PATH + 'wyscout/', WYSCOUT_PATH)
+    unzip_all_files_in_dir(ZIP_PATH + 'skillcorner/', SKILLCORNER_PATH)
+
+# wyscout
+wyscout_data = wyscout_to_df(WYSCOUT_PATH + str(WYSCOUT_ID) + ".json")
+
+# skillcorner
+metadata = pd.read_csv(SKILLCORNER_PATH + str(SKILLCORNER_ID) + "_metadata.csv")
+tracking_df = pd.read_csv(SKILLCORNER_PATH + str(SKILLCORNER_ID) + "_tracking.csv")
+lineup_df = pd.read_csv(SKILLCORNER_PATH + str(SKILLCORNER_ID) + "_lineup.csv")
+
+
+
+# Filter Pass Events
+passes_df = wyscout_data[wyscout_data['type.primary'] == 'pass']
+
+# Filter out inaccurate passes
+accurate_only = True
+if accurate_only:
+    passes_df = passes_df[passes_df['pass.accurate'] == True]
+
+relevant_columns = ['matchId', 'matchTimestamp', 'matchPeriod', 'team.id', 'team.name', 'player.id', 'player.name', 'opponentTeam.id', 'opponentTeam.name', 'pass.recipient.id', 'pass.recipient.name', 'location.x', 'location.y', 'pass.endLocation.x', 'pass.endLocation.y']
+passes_df = passes_df.loc[:,relevant_columns]
+passes_df['matchPeriod'] = passes_df['matchPeriod'].apply(lambda x: int(x.split('H')[0]))
+passes_df['pass.recipient.id'] = passes_df['pass.recipient.id'].astype(int)
+passes_df['pass.endLocation.x'] = passes_df['pass.endLocation.x'].astype(int)
+passes_df['pass.endLocation.y'] = passes_df['pass.endLocation.y'].astype(int)
+
+
+
+# Compute ΔxT
+xt_table = pd.read_csv(XT_PLOT_PATH)
+
+cell_width = 100 / xt_table.shape[1]
+cell_height = 100 / xt_table.shape[0]
+
+def get_xt_index(x, y):
+    x_index = min(int(x // cell_width), xt_table.shape[1] - 1)
+    y_index = min(int(y // cell_height), xt_table.shape[0] - 1)
+    return x_index, y_index
+
+start_xts = passes_df.apply(lambda row: xt_table.iat[get_xt_index(row['location.x'], row['location.y'])[1], 
+                                                           get_xt_index(row['location.x'], row['location.y'])[0]], axis=1)
+end_xts = passes_df.apply(lambda row: xt_table.iat[get_xt_index(row['pass.endLocation.x'], row['pass.endLocation.y'])[1], 
+                                                         get_xt_index(row['pass.endLocation.x'], row['pass.endLocation.y'])[0]], axis=1)
+passes_df.loc[:,'dxt'] = end_xts - start_xts
+
+
+
+# Match skillcorner tracking data with wyscout pass event Data
+tracking_df.drop(columns=['frame_id', 'extrapolated'], inplace=True)   
+tracking_df['timestamp'] = tracking_df.apply(standardize_timestamp, axis=1)
+tracking_df.set_index(['timestamp', 'half'], inplace=True)
+tracking_df.rename_axis(index={'timestamp': 'timestamp', 'half': 'period'}, inplace=True)
+
+passes_df['quantizedTimestamp'] = passes_df['matchTimestamp'].apply(round_to_tenth_of_second)
+passes_df.set_index(['quantizedTimestamp', 'matchPeriod'], inplace=True)
+passes_df.rename_axis(index={'quantizedTimestamp': 'timestamp', 'matchPeriod': 'period'}, inplace=True)
+
+passes_df = passes_df.join(tracking_df, how='inner')
+passes_df.drop(columns=['match_id'], inplace=True)
+passes_df['object_id'] = passes_df['object_id'].astype(int)
+
+
+
+# Normalize Pitch Coordinates
+pitch_length = metadata['pitch_length'].values[0]
+pitch_width = metadata['pitch_width'].values[0]
+
+def normalize_coordinates(row):
+    x, y = row['x'], row['y']
+    new_x = (-x + pitch_length/2) / pitch_length * 100
+    new_y = (y + pitch_width/2) / pitch_width * 100
+    return new_x, new_y
+
+df = passes_df.apply(lambda row: normalize_coordinates(row), axis=1)
+passes_df[['x_norm', 'y_norm']] = pd.DataFrame(df.tolist(), index=df.index)
+
+
+
+# Add Lineup Information
+lineup_df = lineup_df[['team_name', 'player_id', 'player_first_name', 'player_last_name']]
+passes_df = passes_df.merge(lineup_df, left_on='object_id', right_on='player_id')
+columns_to_prefix = ['object_id', 'x', 'y', 'z', 'x_norm', 'y_norm', 'team_name', 'player_id' ,'player_first_name', 'player_last_name']
+prefix = 'tracking.'
+passes_df.rename(columns={col: prefix + col for col in columns_to_prefix}, inplace=True)
+
+if SAVE_PATH:
+    passes_df.to_pickle(SAVE_PATH + 'passes_df.pkl')
+
+with pd.option_context('display.max_columns', None):
+    print(passes_df.head())
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,108 @@
-# Re-defining the function since the environment was reset
+import numpy as np
+import os
+import zipfile
+import json
+import pandas as pd
+from datetime import datetime, timedelta
+
+def round_to_tenth_of_second(timestamp):
+    dt = datetime.strptime(timestamp, '%H:%M:%S.%f')
+    microsecond = dt.microsecond
+    rounded_microsecond = round(microsecond, -5)
+    if rounded_microsecond == 1000000:
+        dt += timedelta(seconds=1)
+        rounded_microsecond = 0
+    dt = dt.replace(microsecond=rounded_microsecond)
+    return f"{dt.strftime('%H:%M:%S')}.{rounded_microsecond:06}"
+
+def standardize_timestamp(row):
+    ts = row['timestamp']
+    if row['half'] == 2:
+        ts += 45 * 60 * 1000
+    td = timedelta(milliseconds=ts)
+    total_seconds = int(td.total_seconds())
+    microseconds = int(td.microseconds)
+    return f"{total_seconds // 3600:02}:{(total_seconds % 3600) // 60:02}:{total_seconds % 60:02}.{microseconds:06}"
+
+def wyscout_to_df(filepath):
+    with open(filepath, encoding='utf8') as f:
+        js = json.load(f)
+    df = pd.json_normalize(js['events'])
+    return df
+
+def unzip_all_files_in_dir(directory, target_directory):
+    for file in os.listdir(directory):
+        if file.endswith('.zip'):
+            with zipfile.ZipFile(directory + file, 'r') as zip_ref:
+                zip_ref.extractall(target_directory)
+
+def responsibility(row, max_distance=10, pass_length_factor=1.0, end_location_factor=0.5):
+    
+    start_x = row['location.x']
+    start_y = row['location.y']
+    end_x = row['pass.endLocation.x']
+    end_y = row['pass.endLocation.y']
+    
+    player_x = row['tracking.x']
+    player_y = row['tracking.y']
+    
+    # Vector from start to end of the pass
+    pass_vector = np.array([end_x - start_x, end_y - start_y])
+    # Vector from start of the pass to the player's position
+    player_vector = np.array([player_x - start_x, player_y - start_y])
+    
+    pass_length = np.linalg.norm(pass_vector)
+    
+    
+    if pass_length > 0:
+        pass_unit_vector = pass_vector / pass_length
+        projection_length = np.dot(player_vector, pass_unit_vector)
+        
+        # Clamp projection_length to the range [0, pass_length] to account for the endpoints of the pass
+        projection_length = max(0, min(projection_length, pass_length))
+        
+        # Find the point on the pass closest to the player
+        closest_point = np.array([start_x, start_y]) + projection_length * pass_unit_vector
+        
+        # Distance from player to the closest point on the pass
+        distance_to_pass = np.linalg.norm(np.array([player_x, player_y]) - closest_point)
+    else:
+        # If the pass length is zero, set distance to the player's distance from the start point
+        distance_to_pass = np.linalg.norm(np.array([player_x, player_y]) - np.array([start_x, start_y]))
+    
+    # Calculate distance from the player to the end location of the pass
+    distance_to_end_location = np.linalg.norm(np.array([player_x, player_y]) - np.array([end_x, end_y]))
+    
+    # Calculate responsibility with pass length and distance to pass scaling
+    if distance_to_pass < max_distance and row['tracking.object_id'] != -1:
+        """
+        # Apply scaling based on the distance to the pass and pass length
+        raw_responsibility = (1 - (distance_to_pass / max_distance)) * np.power(1 + pass_length_factor, pass_length)
+        
+        # Normalize the responsibility to be between 0 and 1
+        max_possible_responsibility = np.power(1 + pass_length_factor, pass_length)
+        responsibility_score = raw_responsibility / max_possible_responsibility
+        
+        # Adjust the responsibility based on the distance to the end location
+        
+        proximity_to_end = 1 - (distance_to_end_location / max_distance)
+        end_location_adjustment = (1 + end_location_factor * proximity_to_end)
+        responsibility_score *= end_location_adjustment
+        """
+        raw_responsibility = (1 - (distance_to_pass / max_distance)) * np.power(1 + pass_length_factor, pass_length)
+        
+        # Normalize the responsibility to be between 0 and 1
+        max_possible_responsibility = np.power(1 + pass_length_factor, pass_length)
+        responsibility_score = raw_responsibility / max_possible_responsibility
+        
+        
+    else:
+        # If the player is too far, they get no responsibility
+        responsibility_score = 0
+        
+    
+        
+    return responsibility_score
 
 def wyscout_to_pitch(x, y, pitch_length=105, pitch_width=68):
     """
